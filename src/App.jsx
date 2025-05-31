e2ae6f3f5d444cb24a425ed172b58dcf23678064
walid <walid.shailkh.3810461@ves.ac.in>
2025-05-31 20:32:37 +0530

    add name test2

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react"
 function App() {
 
 const [name , setName] = useState("");
+
+function handleChange(){
+  setName
+}
   return (
     <>
-     <h1>Hello {name} !</h1>
+     <h1>Hello akil !</h1>
+     {/* <input type="text" onChange={handleChange} /> */}
     </>
   )
 }
